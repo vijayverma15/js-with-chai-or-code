ae0756eefd5426ce969728bbe8e3056dd610b7da
vijayverma15 <vermavijay1511@gmail.com>
2024-07-29 11:29:15 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,10 +10,48 @@
 //"33abc"=>NaN
 //true=>1; false=>0
 
-let isLoggedIn=1
+/*let isLoggedIn=1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn); */
 
 //1=>true; 0=>false
 //""=>false
 //"vijay"=>true
+
+
+
+/**********************Operation******************** */ 
+
+let value =3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1="hello"
+let str2=" vijay"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+//this type of converion in not use at all
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
