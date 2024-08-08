e21436e368c060e36d6c614e0701f70116ef2a32
vijayverma15 <vermavijay1511@gmail.com>
2024-08-08 13:48:11 +0000

    num-maths-done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,48 @@
+const score=400
+
+//console.log(score);
+
+
+const balance=new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);    //conversion in string mathode and /length
+
+//console.log(balance.toFixed(2)); //after the point value how much show 
+  
+
+const otherNumber=23.8966
+
+//console.log(otherNumber.toPrecision(3)); //kitne number tk show krna he  or value fix honi chaiye
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN')); //kisi bhi number ko repsention krne ke liye 
+
+
+
+
+
+//++++++++++++++++++++++++++ MATHS  ++++++++++++++++++++++++++++++++++++++
+
+//console.log(Math);
+
+//console.log(Math.abs(-4)); // this is for abslute value 
+// console.log(Math.round(4.6)); //value ko roundoff krne ke liye
+
+// console.log(Math.ceil(4.2)); //top ki value ke liye
+// console.log(Math.floor(4.9)); // flor value ke liye
+
+// console.log(Math.min(5,8,4,6,2,1,6));
+// console.log(Math.max(5,8,4,6,2,1,6));
+
+
+console.log(Math.random()); //value in o or 1
+console.log((Math.random()*10)+1 );
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
