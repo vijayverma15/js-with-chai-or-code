85baa80985a043588b7c44877a75532b298e952c
vijayverma15 <vermavijay1511@gmail.com>
2024-08-25 10:09:36 +0000

    array done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,47 @@
+//array
+
+const myArr =[0,1,2,3,4,5]
+const myheros=["rajababu", "raju"]
+const myarr2=new Array(1,2,3)
+
+// console.log(myArr[1]);
+
+//Array methods
+
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()  //jo bhi ussending order me he use remove kr dega
+
+
+// console.log(myArr.includes(9));  //koi bhi value dekhne ke liye
+// console.log(myArr.indexOf(3));   //index chake krne ke liye
+
+
+
+// const newArr= myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+//slice ,splice
+
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)// isme range include nhi hoti he 
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3) //ye array se diye hui range ho hata deta hei
+console.log("C",myArr);
+ console.log(myn2); //
+ 
+
+
+
+
