44a68f44b5a3fd5d3c38fd304cbb8e54db655315
vijayverma15 <vermavijay1511@gmail.com>
2024-09-22 12:15:57 +0000

    done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+//singleton  //this is made by contructor not in normle use
+//object.create
+
+//object literals  
+const Jsuser = {
+    name:"vijay",
+    age:18,
+    location:"sehore",
+    email:"vijay@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["monday"]
+}
+
