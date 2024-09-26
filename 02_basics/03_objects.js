910f7454e93950f1ec1a7f442b763a215cfd4ce9
vijayverma15 <vermavijay1511@gmail.com>
2024-09-26 09:42:26 +0000

    done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,12 +2,41 @@
 //object.create
 
 //object literals  
+const mysym=Symbol("key1")
 const Jsuser = {
     name:"vijay",
+    "full name":"vijay verma",
+   [mysym]:"mykey1",  //[] use krne sw symbol pass hota hai nhi krne se string
     age:18,
     location:"sehore",
     email:"vijay@google.com",
     isLoggedIn:false,
-    lastLoginDays:["monday"]
+    lastLoginDays:["monday","saturday"]
 }
+
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);
+// console.log(Jsuser["full name"]);
+// console.log(  Jsuser[mysym])
+
+
+Jsuser.email="vijay@micro.com"  //value change krne k eliye
+//Object.freeze(Jsuser)  // iske bad koi change nhi hoga
+Jsuser.email="vijay@chatgpt"
+// console.log(Jsuser);
+
+
+Jsuser.greeting= function(){
+    console.log("hello jsuser");
+    
+}
+
+
+Jsuser.greetingtwo= function(){
+    console.log(`hello jsuser,${this.name}`);
+    
+}
+
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingtwo());
 
